fdf350fb147ac875332d4a40699f61454840a3df
David Diggs <ddiggs34@gmail.com>
2023-11-13 16:00:19 -0500

    done

--- Testing/IProductRepo.cs
+++ Testing/IProductRepo.cs
@@ -6,7 +6,15 @@ namespace Testing
 {
     public interface IProductRepo
     {
+      
         public IEnumerable<Product> GetAllProducts();
+        public Product GetProduct(int id);
+        public void UpdateProduct(Product product);
+
+        public void InsertProduct(Product productToInsert);
+        public IEnumerable<Category> GetCategories();
+        public Product AssignCategory();
+        public void DeleteProduct(Product product);
 
     }
 }
--- Testing/ProductRepo.cs
+++ Testing/ProductRepo.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Data;
 using Dapper;
+using Testing.Models;
+using System;
 
 namespace Testing
 {
@@ -12,9 +14,49 @@ namespace Testing
             _conn = conn;
         }
 
+        public Product AssignCategory()
+        {
+            var categoryList = GetCategories();
+            var product = new Product();
+            product.Categories = categoryList;
+            return product;
+        }
+
         public IEnumerable<Product> GetAllProducts()
         {
             return _conn.Query<Product>("SELECT * FROM PRODUCTS;");
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            return _conn.Query<Category>("SELECT * FROM categories;");
+        }
+
+        public Product GetProduct(int id) 
+        {
+            return _conn.QuerySingle<Product>("SELECT * FROM PRODUCTS WHERE PRODUCTID = @id",
+                new { id = id });
+        }
+
+        
+
+        public void UpdateProduct(Product product)
+        {
+            _conn.Execute("UPDATE products SET Name = @name, Price = @price WHERE ProductID = @id",
+                new { name = product.Name, price = product.Price, id = product.ProductID });
+        }
+
+       void IProductRepo.DeleteProduct(Product product)
+        {
+            _conn.Execute("DELETE FROM REVIEWS WHERE ProductID = @id;", new { id = product.ProductID });
+            _conn.Execute("DELETE FROM Sales WHERE ProductID = @id;", new { id = product.ProductID });
+            _conn.Execute("DELETE FROM Products WHERE ProductID = @id;", new { id = product.ProductID });
+        }
+
+        void IProductRepo.InsertProduct(Product productToInsert)
+        {
+            _conn.Execute("INSERT INTO products (NAME, PRICE, CATEGORYID) VALUES (@name, @price, @categoryID);",
+                new { name = productToInsert.Name, price = productToInsert.Price, categoryID = productToInsert.CategoryID });
         }
     }
 }
